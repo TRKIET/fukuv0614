77e7eecd668132329f5628256ce5f4109477cc48
katann-IWATATE-tann <104120192+katann-tann@users.noreply.github.com>
2024-06-14 13:53:48 +0900

    復習問題

--- fukuv0614/Form1.cs
+++ fukuv0614/Form1.cs
@@ -1,10 +1,42 @@
 namespace fukuv0614
 {
+
     public partial class Form1 : Form
     {
+        static Random random = new Random();
+
+        int ans = random.Next(51);
+        int count = 0;
+
+
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int yosou;
+
+
+
+            count++;
+
+            MessageBox.Show($"{count}‰ñ–Ú‚Ì’§í");
+
+            if (ans == yosou)
+            {
+                MessageBox.Show("Bingo!!");
+                count = 0;
+            }
+            else if (ans<yosou) 
+            {
+                MessageBox.Show("Less");
+            }
+            else
+            {
+                MessageBox.Show("Greater");
+            }
         }
     }
 }
